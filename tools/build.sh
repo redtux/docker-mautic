f14468d5036f50a68ef4aecdf0f0cbb2a9940fcb
Pablo Hörtner <redtux@pm.me>
2023-12-11 00:48:03 +0100

    build(tools): Update build script

--- tools/build.sh
+++ tools/build.sh
@@ -2,10 +2,10 @@
 # vim: set ai expandtab sw=2 ts=2 sts=2 re=2:
 #
 # SPDX-FileCopyrightText: 2023 Pablo Hörtner <redtux@pm.me>
-# SPDX-License-Identifier: AGPL-3.0-or-later
+# SPDX-License-Identifier: GPL-3.0-or-later
 #
 # Copyright © 2023 by Pablo Hörtner <redtux@pm.me>. All rights reserved.
-# You can find the AGPL v3.0 here: https://www.gnu.org/licenses/agpl-3.0
+# You can find the GPL v3.0 here:  https://www.gnu.org/licenses/gpl-3.0
 # Attribution required if you use this file (or parts) in your projects.
 #
 # ./build.sh -- Build images
@@ -14,36 +14,12 @@
 IFS=$'\n\t'
 set -euo pipefail
 
-# Set log title color
-log_title() {
-  local color='\033[1;35m'  # Purple
-  local nc='\033[0m'
-  printf "%s \n${color}${1}${nc}\n"
-}
+# Include common functions
+source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 
-# Set title color
-title() {
-  local color='\033[1;34m'  # Light White
-  local nc='\033[0m'
-  printf "%s \n${color}${1}${nc}\n"
-}
-
-# Function for consistent messaging
-heading_msg() {
-  title "==== 🔨 Building Mautic Image · $1 💫 ===="
-  echo
-}
-
-# Function for successful build
-build_success() {
-  title "✅ Build successful 🙂"
-}
-
-# Function for failed build
-build_failed() {
-  title "❎ Build failed 😵"
-  exit 1
-}
+# Set working directory to parent directory of the script
+WORKDIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")/.."
+cd "$WORKDIR"
 
 # Set environment variables
 ENV_FILE="${PWD}/.env"
@@ -63,17 +39,8 @@ echo
 # Log end of update script
 log_title "==== 🌠 Updating sources finished 🎉 ===="
 
-# Check for Podman Compose
-docker-compose() {
-  if command -v podman-compose >/dev/null; then
-    podman-compose "$@"
-  else
-    command docker-compose "$@"
-  fi
-}
-
 # Build Mautic Image - Apache HTTP
-heading_msg "Apache HTTP"
+build_title "Apache HTTP"
 if docker-compose -f apache/compose.yaml build; then
   build_success
 else
@@ -82,7 +49,7 @@ fi
 echo
 
 # Build Mautic Image - PHP FPM
-heading_msg "PHP FPM"
+build_title "PHP FPM"
 if docker-compose -f fpm/compose.yaml build; then
   build_success
 else
