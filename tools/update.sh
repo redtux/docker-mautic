a2eb78191524533c266cb2c0ec83481993ddc764
Pablo Hörtner <redtux@pm.me>
2023-12-11 15:32:20 +0100

    build(tools): Override MAUTIC_VERSION in update.sh
    Updated the "tools/update.sh" script to consume externally
    defined "MAUTIC_VERSION", eg. as environment variable, for
    better integration with the new "tools/build.sh" script...
    
    - Check if "MAUTIC_VERSION" exists, and pass its value
    - Fix inkonsistent indentation (using two spaces not tabs)
    - Set working directory to project root (instead of "tools/")
    - Add copyright and license header
    - Enable strict mode

--- tools/update.sh
+++ tools/update.sh
@@ -1,24 +1,44 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+# vim: set ai expandtab sw=2 ts=2 sts=2 re=2:
+#
+# SPDX-FileCopyrightText: 2015 Michael Babker <michael.babker@mautic.org>
+# SPDX-FileCopyrightText: 2023 Pablo Hörtner <redtux@pm.me>
+# SPDX-License-Identifier: GPL-3.0-or-later
+# You can find the GPL v3.0 here:  https://www.gnu.org/licenses/gpl-3.0
+#
+# ./update.sh -- Update image sources
 
-current=$(curl https://api.github.com/repos/mautic/mautic/releases/latest -s | jq -r '.name | sub("Mautic Community "; "")')
+# Enable strict mode
+IFS=$'\n\t'
+set -euo pipefail
+
+# Set working directory to parent directory of the script
+WORKDIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")/.."
+cd "$WORKDIR"
+
+if [ -z "$MAUTIC_VERSION" ]; then
+  latest_api_url="https://api.github.com/repos/mautic/mautic/releases/latest"
+  current=$(curl -Ls "$latest_api_url" | jq -r '.name | sub("Mautic Community "; "")')
+else
+  current="$MAUTIC_VERSION"
+fi
 
 # TODO - Expose SHA signatures for the packages somewhere
 wget -O mautic.zip https://github.com/mautic/mautic/releases/download/$current/$current.zip
 sha1="$(sha1sum mautic.zip | sed -r 's/ .*//')"
 
 for variant in apache fpm; do
-	(
-		set -x
-
-		sed -ri '
-			s/^(ENV MAUTIC_VERSION) .*/\1 '"$current"'/;
-			s/^(ENV MAUTIC_SHA1) .*/\1 '"$sha1"'/;
-		' "$variant/Dockerfile"
-
+  (
+    set -x
+  
+    sed -ri '
+      s/^(ENV MAUTIC_VERSION) .*/\1 '"$current"'/;
+      s/^(ENV MAUTIC_SHA1) .*/\1 '"$sha1"'/;
+    ' "$variant/Dockerfile"
+  
         # To make management easier, we use these files for all variants
-		cp common/* "$variant"/
-	)
+    cp common/* "$variant"/
+  )
 done
 
 rm mautic.zip
